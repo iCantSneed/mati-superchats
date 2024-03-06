f29a6342d3aa7237ec89ab9afa94ca162bfa3386
iCantSneed <140197533+iCantSneed@users.noreply.github.com>
2024-03-06 03:40:07 +0000

    Run post-deploy actions

--- src/Controller/DeployController.php
+++ src/Controller/DeployController.php
@@ -4,20 +4,29 @@ declare(strict_types=1);
 
 namespace Mati\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\StreamOutput;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DeployController extends AbstractController
 {
   #[Route('/deploy', methods: ['POST'], condition: "request.headers.get('X-Mati-Deploy') === env('APP_SECRET')")]
-  public function deploy(Request $request): Response
+  public function deploy(Request $request, KernelInterface $kernel): Response
   {
     $archive = $request->files->get('archive');
-    \assert($archive instanceof UploadedFile);
+    if (!$archive instanceof UploadedFile) {
+      echo "Archive is not UploadedFile???\n";
+
+      throw new \Exception();
+    }
+
     $zipFile = new \ZipArchive();
     $result = $zipFile->open($archive->getPathname());
     if (true !== $result) {
@@ -28,7 +37,7 @@ class DeployController extends AbstractController
 
     $response = new StreamedResponse();
     $response->headers->set('Content-Type', 'text/plain');
-    $response->setCallback(static function () use ($zipFile): void {
+    $response->setCallback(static function () use ($zipFile, $kernel): void {
       $dest = \dirname(__FILE__, 3);
       for ($i = 0; $i < $zipFile->numFiles; ++$i) {
         if (0 === $i % 100) {
@@ -43,8 +52,26 @@ class DeployController extends AbstractController
           throw new \Exception();
         }
       }
+      $zipFile->close();
       echo "Extracted all files\n";
       static::flush();
+
+      $commands = [
+        'cache:clear' => ['--env' => 'prod'],
+        'doctrine:migrations:migrate' => [],
+      ];
+      $application = new Application($kernel);
+      $application->setAutoExit(false);
+
+      $output = new StreamOutput(fopen('php://stdout', 'w'));
+      foreach ($commands as $command => $args) {
+        $input = new ArrayInput(['command' => $command, ...$args]);
+        $result = $application->run($input, $output);
+        static::flush();
+        if (0 !== $result) {
+          throw new \Exception();
+        }
+      }
     });
 
     return $response;
