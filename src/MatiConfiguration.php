2ae7400f3756e6dfe9b933f0a86b2456092267bc
iCantSneed <140197533+iCantSneed@users.noreply.github.com>
2024-03-25 14:21:28 +0000

    Use PHP 8.3 features
    Closes #38.

--- src/MatiConfiguration.php
+++ src/MatiConfiguration.php
@@ -6,9 +6,9 @@ namespace Mati;
 
 final readonly class MatiConfiguration
 {
-  public const IPC_ADDRESS = '127.255.255.255';
+  public const string IPC_ADDRESS = '127.255.255.255';
 
-  public const PARAM_LIVESTREAM_LANDING_URL = '%env(LIVESTREAM_LANDING_URL)%';
-  public const PARAM_LIVESTREAM_RSS_URL = '%env(LIVESTREAM_RSS_URL)%';
-  public const PARAM_IPC_PORT = '%env(IPC_PORT)%';
+  public const string PARAM_LIVESTREAM_LANDING_URL = '%env(LIVESTREAM_LANDING_URL)%';
+  public const string PARAM_LIVESTREAM_RSS_URL = '%env(LIVESTREAM_RSS_URL)%';
+  public const string PARAM_IPC_PORT = '%env(IPC_PORT)%';
 }
